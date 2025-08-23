ca0beeaf24ba85d6a0e4f699ad1738197d2db02e
lakshita <lakshita21lr@gmail.com>
2025-08-23 13:12:46 +0530

    message2

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import Index from "./pages/Index";
 import ARViewer from "./pages/ARViewer";
 import AllCollections from "./pages/AllCollections";
 import CollectionDetail from "./pages/CollectionDetail";
+import AllArtists from "./pages/AllArtists";
+import ArtistProfile from "./pages/ArtistProfile";
 import NotFound from "./pages/NotFound";
 import AuthDemo from "./pages/AuthDemo";
 
@@ -29,6 +31,8 @@ const App = () => (
               <Route path="/ar-viewer" element={<ARViewer />} />
               <Route path="/collections" element={<AllCollections />} />
               <Route path="/collections/:id" element={<CollectionDetail />} />
+              <Route path="/artists" element={<AllArtists />} />
+              <Route path="/artists/:id" element={<ArtistProfile />} />
               <Route path="/auth-demo" element={<AuthDemo />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
